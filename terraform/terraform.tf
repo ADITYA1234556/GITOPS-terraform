672b6c8493a3de2965ca353944c30e5679d2a469
aditya1234556 <adityanavaneethan98@gmail.com>
2024-08-25 15:04:56 +0100

    bucket name and cluster name updated

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "kubernetes-kops-cluster"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "eu-west-2"
   }
 
   required_version = "~> 1.6.3"
